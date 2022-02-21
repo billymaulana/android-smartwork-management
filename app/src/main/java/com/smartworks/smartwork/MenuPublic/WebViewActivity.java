02d49cf713304bb948626a8217ed95686f1e2b98
ilhamramaadhan74@gmail.com <vdtfpqmb08>
2022-02-21 22:21:51 +0700

    Fixing several things

---
+++ app/src/main/java/com/smartworks/smartwork/MenuPublic/WebViewActivity.java
@@ -0,0 +1,39 @@
+package com.smartworks.smartwork.MenuPublic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.smartworks.smartwork.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @BindView(R.id.webview)
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        ButterKnife.bind(this);
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        // Tiga baris di bawah ini agar laman yang dimuat dapat
+        // melakukan zoom.
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        // Baris di bawah untuk menambahkan scrollbar di dalam WebView-nya
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSfC6AHQWWGvDIuCb1XTA7xTEAWlhUnfPudWNAsv1ffzvqBa8A/viewform");
+    }
+}
